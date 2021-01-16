7e8a37f911de31c553391e9100516730d5771ffb
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-01-16 14:56:59 -0600

    debugdanHELP
    why dis happen

---
+++ script.js
@@ -0,0 +1,36 @@
+var keynote = "before"
+var list = document.getElementById(keynote)
+var input = document.getElementById("inputbox")
+
+
+function keywhich(event) {
+	 
+	if (event.which === 48) {
+		keynote = "before"
+		return keynote
+	} else if (event.which === 49) {
+		keynote = "during"
+		return keynote
+	} else if (event.which === 50) {
+		keynote = "after"
+		return keynote
+	}
+}
+
+
+function createListElement(keynote) {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(`<a href="${keynote}.html">${keynote}.html</a>`));
+	
+	list.appendChild(li);
+	console.log("created list", list)
+}
+
+function addListAfterKeypress(event) {
+		keywhich(event);
+		createListElement(event);
+		console.log("pressed key", event.key,  " works!", event.which);
+		input.value = "";
+}
+
+input.addEventListener("keypress", addListAfterKeypress)
